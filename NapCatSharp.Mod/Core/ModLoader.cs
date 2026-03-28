de4ed4ae998eec560ff2484e3144022d4836a744
Nan_Ting <a3380991070@163.com>
2026-03-28 15:27:36 +0800

    MVC: ModLoader添加关于Mod文件的辅助方法

--- NapCatSharp.Mod/Core/ModLoader.cs
+++ NapCatSharp.Mod/Core/ModLoader.cs
@@ -1,9 +1,10 @@
-﻿using NapCatSharp.OB11.OB11MessageModels;
-
-namespace NapCatSharp.Mod.Core;
+﻿namespace NapCatSharp.Mod.Core;
 
 public static class ModLoader
 {
+    /// <summary>
+    /// 加载全部模组
+    /// </summary>
     internal static void LoadMods()
     {
         // 仅包含名称
@@ -22,6 +23,9 @@ public static class ModLoader
         }
     }
 
+    /// <summary>
+    /// 加载给定模组
+    /// </summary>
     internal static bool LoadMod(string modName)
     {
         var moddir = Path.Combine(ModContext.ModPath, modName);
@@ -47,7 +51,7 @@ public static class ModLoader
     }
 
     /// <summary>
-    /// 本地可启用模组
+    /// 获取全部本地可启用模组
     /// </summary>
     internal static List<string> LocalMods()
     {
@@ -82,8 +86,63 @@ public static class ModLoader
         return true;
     }
 
+    /// <summary>
+    /// 禁用给定模组
+    /// </summary>
     internal static void DisableMod(string modName)
     {
         ModContext.UnLoadMod(modName);
+    }
+
+    /// <summary>
+    /// 将数据覆盖写入到模组文件中
+    /// </summary>
+    internal static async Task OverrideModFileAsync(string modName, string fileName, byte[] bytes)
+    {
+        var tarFileName = FileCreate(modName, fileName);
+        var fileStream = File.Create(tarFileName);
+        await fileStream.WriteAsync(bytes);
+        fileStream.Dispose();
+    }
+
+    /// <summary>
+    /// 将数据追加到模组文件中
+    /// </summary>
+    internal static async Task AppendModFileAsync(string modName, string fileName, byte[] bytes)
+    {
+        var tarFileName = FileCreate(modName, fileName);
+        var fileWrite = File.OpenWrite(tarFileName);
+        await fileWrite.WriteAsync(bytes);
+        fileWrite.Dispose();
+    }
+
+    /// <summary>
+    /// 删除给定模组的全部文件 含文件夹
+    /// <param name="modName">模组名称</param>
+    /// <param name="recursive"> 是否递归删除 </param>
+    /// </summary>
+    internal static void DeleteModFiles(string modName, bool recursive = true)
+    {
+        var modDirName = Path.Combine(ModContext.ModPath, modName);
+        Directory.Delete(modDirName, recursive);
+    }
+
+    /// <summary>
+    /// 创建给定的文件，Mods/<paramref name="modName"/>/<paramref name="fileName"/>
+    /// </summary>
+    /// <param name="modName">模组名称</param>
+    /// <param name="fileName">文件名称</param>
+    /// <returns></returns>
+    internal static string FileCreate(string modName, string fileName)
+    {
+        var modDirName = Path.Combine(ModContext.ModPath, modName);
+        var tarFileName = Path.Combine(modDirName, fileName);
+        if (!Directory.Exists(modDirName)) {
+            Directory.CreateDirectory(modDirName);
+        }
+        if (!File.Exists(tarFileName)) {
+            File.Create(tarFileName).Dispose();
+        }
+        return tarFileName;
     }
 }
